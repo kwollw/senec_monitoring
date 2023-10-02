eb1fd6fb86889629709bdd0e605bc51eeedeef41
Kai Wollweber <wollw@ki.tng.de>
2023-10-02 21:13:40 +0200

    change current status

--- senec.py
+++ senec.py
@@ -75,17 +75,8 @@ class Senec():
         return data
 
 BASIC_REQUEST = {
-    'STATISTIC': {
-        'CURRENT_STATE': ''                # Current state of the system (int, see SYSTEM_STATE_NAME)
-        #'LIVE_BAT_CHARGE_MASTER': '',       # Battery charge amount since installation (kWh)
-        #'LIVE_BAT_DISCHARGE_MASTER': '',    # Battery discharge amount since installation (kWh)
-        #'LIVE_GRID_EXPORT': '',             # Grid export amount since installation (kWh)
-        #'LIVE_GRID_IMPORT': '',             # Grid import amount since installation (kWh)
-        #'LIVE_HOUSE_CONS': '',              # House consumption since installation (kWh)
-        #'LIVE_PV_GEN': '',                  # PV generated power since installation (kWh)
-        #'MEASURE_TIME': ''                  # Unix timestamp for above values (ms)
-    },
     'ENERGY': {
+        'STAT_STATE': '',                 # Current state of the system (int, see SYSTEM_STATE_NAME)
         #'GUI_BAT_DATA_CURRENT': '',         # Battery charge current: negative if discharging, positiv if charging (A)
         'GUI_BAT_DATA_FUEL_CHARGE': '',     # Remaining battery (percent)
         'GUI_BAT_DATA_POWER': '',           # Battery charge power: negative if discharging, positiv if charging (W)
